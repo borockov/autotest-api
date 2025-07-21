e8dad645cb49bcd966f94a898a9be17c2f018b37
borockov <yaborockov@gmail.com>
2025-07-21 13:07:46 +0300

    8.5 Фикстуры Pytest

--- autotest-api/tests/test_authentication.py
+++ autotest-api/tests/test_authentication.py
@@ -1,34 +1,24 @@
-# autotests-api/tests/test_authentication.py
 
 from http import HTTPStatus
 
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.users.users_schema import CreateUserRequestSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
-
-from clients.users.public_users_client import get_public_users_client
-from clients.authentication.authentication_client import get_authentication_client
+from tools.assertions.schema import validate_json_schema
+from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
 import pytest
 
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login():
-    users_client = get_public_users_client()
-    auth_client = get_authentication_client()
+def test_login(function_user: UserFixture, authentication_client: AuthenticationClient):
+    request = LoginRequestSchema(email=function_user.email,password=function_user.password)
+    response = authentication_client.login_api(request)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    user_data = CreateUserRequestSchema()
-    users_client.create_user_api(user_data)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_login_response(response_data)
 
-    login_request = LoginRequestSchema(
-        email=user_data.email,
-        password=user_data.password
-    )
+    validate_json_schema(response.json(), response_data.model_json_schema())
 
-    login_response = auth_client.login_api(login_request)
-
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
-
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
-
-    assert_login_response(login_response_data)
