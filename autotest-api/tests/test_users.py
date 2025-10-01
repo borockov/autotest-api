72ceb1df58788419816fa69db79437774a355e9b
borockov <yaborockov@gmail.com>
2025-10-01 16:32:12 +0300

    8.7 параметризация Pytest,исправление

--- autotest-api/tests/test_users.py
+++ autotest-api/tests/test_users.py
@@ -14,11 +14,9 @@ from tools.fakers import fake
 
 @pytest.mark.users
 @pytest.mark.regression
-@pytest.mark.parametrize("email", [fake.email("mail.ru"), fake.email("gmail.com"), fake.email("example.com")])
+@pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
 def test_create_user(email: str, public_users_client: PublicUsersClient):
-    request = CreateUserRequestSchema(
-        email=email
-    )
+    request = CreateUserRequestSchema(email=fake.email(domain=email))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
